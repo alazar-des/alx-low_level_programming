00c9eb509decf7aaf0d45ee0d6f0924030130057
alazar-des <alazar.des@gmail.com>
2021-02-12 09:04:38 +0000

    int min fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
+#define INT_MIN -2147483648
 
 /**
  * _abs - return absoute number of an intiger
- * @n - input intiger
+ * @n: input intiger
  *
  * Return: absolute value
  */
@@ -12,7 +13,7 @@ int _abs(int n)
 		return (-1 * n);
 	else
 		return (n);
-} 
+}
 /**
  * print_number - print number using putchar
  * @n: intiger to be printed
@@ -20,11 +21,11 @@ int _abs(int n)
  */
 
 void print_number(int n)
-{	
+{
 	if (n < 0)
 		_putchar('-');
 	if (_abs(n / 10))
 		print_number(_abs(n / 10));
 
-	_putchar('0' + _abs(n) % 10);
+	_putchar('0' + _abs(n % 10));
 }
