6fc60a0396a4df3c7991243b81c0c6516c8413ed
alazar-des <alazar.des@gmail.com>
2021-02-12 08:48:05 +0000

    min int fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,20 +1,30 @@
 #include "holberton.h"
 
 /**
+ * _abs - return absoute number of an intiger
+ * @n - input intiger
+ *
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+} 
+/**
  * print_number - print number using putchar
  * @n: intiger to be printed
  *
  */
 
 void print_number(int n)
-{
+{	
 	if (n < 0)
-	{
 		_putchar('-');
-		n = -n;
-	}
-	if (n / 10)
-		print_number(n / 10);
+	if (_abs(n / 10))
+		print_number(_abs(n / 10));
 
-	_putchar('0' + n % 10);
+	_putchar('0' + _abs(n) % 10);
 }
