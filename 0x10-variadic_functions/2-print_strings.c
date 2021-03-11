612762fcbaa072bd2f2f23b37b1db4721458657c
alazar-des <alazar.des@gmail.com>
2021-03-11 18:08:59 +0300

    negative n

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,7 +14,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	char *str;
 
-	if (n == 0)
+	if (n <= 0)
 		return;
 	va_start(args, n);
 	if (separator == NULL)
