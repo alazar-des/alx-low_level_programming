d636739ede1b080e031a9e9eeb06323b50202301
alazar-des <alazar.des@gmail.com>
2021-03-19 17:15:22 +0300

    strdup not permited

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,6 +1,45 @@
 #include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+
+/**
+ * _strlen - return length of string
+ * @str: string pointer
+ *
+ * Return: length of string
+ */
+size_t _strlen(const char *str)
+{
+	size_t count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+/**
+ * _strdup - duplicate string
+ * @src: string to be duplicated
+ *
+ * Return: duplicated pointer in success, otherwise NULL
+ */
+char *_strdup(const char *src)
+{
+	size_t size, i;
+	char *dest;
+
+	size = _strlen(src);
+	dest = malloc(size + 1);
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * add_node - add a new node at the begining
@@ -18,8 +57,13 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = strlen(new->str);
+	new->str = _strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
 	new->next = *head;
 	*head = new;
 	return (*head);
