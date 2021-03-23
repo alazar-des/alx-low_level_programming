0eae7f5a29c4f0751cfcf8be753bac2fb154b261
alazar-des <alazar.des@gmail.com>
2021-03-23 17:02:21 +0300

    printf forbiden

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,21 @@
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+
+/**
+ * _prntf - print integer to standard output
+ * @n: input integer
+ *
+ */
+void _printf(size_t n)
+{
+	char c;
+
+	if (n / 10)
+		_printf(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);
+}
 
 /**
  * print_listint - print all elements of node in a list
@@ -11,6 +26,7 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t len;
+	char nl = '\n';
 
 	len = 0;
 	if (h != NULL || h->next != NULL)
@@ -18,10 +34,14 @@ size_t print_listint(const listint_t *h)
 		for (; h->next != NULL; h = h->next)
 		{
 			if (h != NULL)
-				printf("%d\n", h->n);
+			{
+				_printf(h->n);
+				write(1, &nl, 1);
+			}
 			len++;
 		}
-		printf("%d\n", h->n);
+		_printf(h->n);
+		write(1, &nl, 1);
 		len++;
 	}
 	return (len);
