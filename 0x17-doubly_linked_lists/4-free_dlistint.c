2db2203c8d2a22c92ab4f2caf0867d6754d48580
alazar-des <alazar.des@gmail.com>
2021-05-02 17:55:28 +0300

    free doubly linked list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -2,6 +2,30 @@
 #include "lists.h"
 
 /**
+ * find_last_node - returns the last node
+ * @h: pointer to the head node
+ *
+ * Return: the last node
+ */
+const dlistint_t *find_last_nodefr(const dlistint_t *h)
+{
+	const dlistint_t *head = h, *end = NULL, *temp;
+
+	while (h != NULL)
+	{
+		temp = head;
+		while (temp != h)
+		{
+			if (h->next == temp)
+				return (h);
+			temp = temp->next;
+		}
+		end = h;
+		h = h->next;
+	}
+	return (end);
+}
+/**
  * free_dlistint - free doubly linked list
  * @head: pointer to the head node
  *
@@ -13,7 +37,7 @@ void free_dlistint(dlistint_t *head)
 
 	if (head == NULL)
 		return;
-	end = find_last_node(head);
+	end = find_last_nodefr(head);
 	while (head != end)
 	{
 		next = head->next;
