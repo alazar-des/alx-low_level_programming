c33ad0ad0a4cf78169cd63624474e2a409eba471
alazar-des <alazar.des@gmail.com>
2021-05-28 13:34:01 +0300

    retrive data from hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * _strcmpg - compare two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: if s1 > s2 return positive, if s1 < s2 negative, otherwise 0
+ */
+int _strcmpg(char *s1, const char *s2)
+{
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 - *s2 != 0)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	return (0);
+}
+
+/**
+ * has_table_get - get value from hash table
+ * @key: the key to look for
+ *
+ * Return: value if key exists, otherwise NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (NULL);
+	index = key_index((unsigned char *)key, ht->size);
+	if (ht->array[index] == 0)
+		return (NULL);
+	while (ht->array[index] != NULL)
+	{
+		if (_strcmpg(ht->array[index]->key, key) == 0)
+			return (ht->array[index]->value);
+		ht->array[index] = ht->array[index]->next;
+	}
+	return (NULL);
+}
