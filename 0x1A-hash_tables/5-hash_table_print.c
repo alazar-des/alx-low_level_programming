1e919a1c8a3959dfb72daea4c66b53cdd5303871
alazar-des <alazar.des@gmail.com>
2021-05-28 14:11:48 +0300

    print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,7 +9,6 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	int flag = 0;
 	hash_node_t *next;
 
 	if (ht != NULL)
@@ -20,13 +19,11 @@ void hash_table_print(const hash_table_t *ht)
 			for (unsigned long int i = 0; i < ht->size; i++)
 			{
 				next = ht->array[i];
-				while (next != 0)
+				while (next != NULL)
 				{
-					if (flag)
-						printf(", ");
+					printf(", ");
 					printf("'%s': '%s'", next->key,
 					       next->value);
-					flag = 1;
 					next = next->next;
 				}
 			}
