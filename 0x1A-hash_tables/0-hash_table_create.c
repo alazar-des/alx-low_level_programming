d8ad10122b17585e98f066549ade6121e93584a5
alazar-des <alazar.des@gmail.com>
2021-05-28 14:06:07 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ht = calloc(1, sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = calloc(size ,sizeof(hash_node_t *));
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
 		return (NULL);
 	ht->size = size;
