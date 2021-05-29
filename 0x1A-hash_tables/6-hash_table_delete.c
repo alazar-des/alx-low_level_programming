5b49d3c5104841758311a1beadcb345e61e07393
alazar-des <alazar.des@gmail.com>
2021-05-29 11:03:48 +0300

    delete hash table

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,22 +9,23 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	hash_node_t *next;
+	unsigned long int i;
 
 	if (ht != NULL)
 	{
-		//if (ht->array != NULL)
+		if (ht->array != NULL)
 		{
-			for (unsigned long int i = 0; i < ht->size; i++)
+			for (i = 0; i < ht->size; i++)
 			{
 				while (ht->array[i] != NULL)
 				{
 					next = ht->array[i]->next;
-					//if (ht->array[i]->key != NULL)
+					if (ht->array[i]->key != NULL)
 					{
 						free(ht->array[i]->key);
 						ht->array[i]->key = NULL;
 					}
-					//if (ht->array[i]->value != NULL)
+					if (ht->array[i]->value != NULL)
 					{
 						free(ht->array[i]->value);
 						ht->array[i]->value = NULL;
