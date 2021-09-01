89e36e163a701ff4a6ee1b5523b047b96a42575e
alazar-des <alazar.des@gmail.com>
2021-09-01 11:47:29 +0300

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef HF
+#define HF
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif
