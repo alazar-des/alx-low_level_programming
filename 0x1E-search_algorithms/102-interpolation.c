f3044d6299b0a206f2be3bd75e3fd460fb1365ff
alazar-des <alazar.des@gmail.com>
2021-09-02 19:13:01 +0300

    interplation search algorithm

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_recursive - interpolation search implementation
+ * @array: an array which a value will be searched in
+ * @lo: begining of array index
+ * @hi: end index of array to be searched
+ * @value: a value to be searched
+ *
+ * Return: index of the value if exists, otherwise -1
+ */
+int interpolation_recursive(int *array, size_t lo, size_t hi, int value)
+{
+	size_t pos;
+
+	pos = lo + ((value - array[lo]) * (hi - lo)
+			   / (array[hi] - array[lo]));
+	if (lo <= hi && value >= array[lo] && value <= array[hi])
+	{
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] > value)
+			return (interpolation_recursive(array, lo, pos, value));
+		else
+			return (interpolation_recursive(array, pos, hi, value));
+	}
+	else
+		printf("Value checked array[%ld] is out of range\n", pos);
+	return (-1);
+}
+
+/**
+ * interpolation_search - interpolation search implementation
+ * @array: an array which a value will be searched in
+ * @size: size of array
+ * @value: a value to be searched
+ *
+ * Return: index of the value if exists, otherwise -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	return (interpolation_recursive(array, 0, size - 1, value));
+}
